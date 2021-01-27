f7ec47ae45256b4645446eb3afcd24c9f01a2af0
renatogual <renatogual@hotmail.com>
2021-01-26 22:09:21 -0300

    Finalizando configs e habilitando o cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,10 +3,14 @@ const port = 3000
 const bodyParser = require('body-parser') //Faz o parser das informações recebidas da requisição, transforma em json ou em string
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`Backend está rodando na porta ${port}`);
 })
+
+module.exports = server
