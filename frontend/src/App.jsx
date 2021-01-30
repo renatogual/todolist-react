1e92f29368b10b0892369c6c85a0b20ec1c203d4
renatogual <renatogual@hotmail.com>
2021-01-30 18:48:08 -0300

    Criando componentes todo,menu e about

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+import Todo from './todo/Todo'
+import About from './about/About'
+import Menu from './template/Menu'
 
 export default () => (
   <div className="container">
-    <h1>teste</h1>
+    <Menu />
+    <Todo />
+    <About />
   </div>
-);
+)
