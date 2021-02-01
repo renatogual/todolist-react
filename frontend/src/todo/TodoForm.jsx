8ac75ce79cc40104b89dd6682bf9285f07ab016d
renatogual <renatogual@hotmail.com>
2021-02-01 15:57:56 -0300

    Criando atalhos

--- frontend/src/todo/TodoForm.jsx
+++ frontend/src/todo/TodoForm.jsx
@@ -2,25 +2,51 @@ import React from 'react'
 import Grid from '../template/Grid'
 import IconButton from '../template/IconButton'
 
-export default ({ handleChange, handleAdd, handleSearch, description }) => (
-  <div role="form" className="todoForm">
-    <Grid cols="12 9 10">
-      <input
-        onChange={handleChange}
-        value={description}
-        type="text"
-        placeholder="Adicione uma tarefa"
-        id="description"
-        className="form-control"
-      />
-    </Grid>
-    <Grid cols="12 3 2">
-      <IconButton style="primary" icon="plus" onClick={handleAdd}></IconButton>
-      <IconButton
-        style="info"
-        icon="search"
-        onClick={handleSearch}
-      ></IconButton>
-    </Grid>
-  </div>
-)
+export default ({
+  handleChange,
+  handleAdd,
+  handleSearch,
+  handleClear,
+  description,
+}) => {
+  function keyHandler(e) {
+    if (e.key === 'Enter') {
+      // eslint-disable-next-line no-unused-expressions
+      e.shiftKey ? handleSearch() : handleAdd()
+    } else if (e.key === 'Escape') {
+      handleClear()
+    }
+  }
+  return (
+    <div role="form" className="todoForm">
+      <Grid cols="12 9 10">
+        <input
+          onChange={handleChange}
+          onKeyUp={keyHandler}
+          value={description}
+          type="text"
+          placeholder="Adicione uma tarefa"
+          id="description"
+          className="form-control"
+        />
+      </Grid>
+      <Grid cols="12 3 2">
+        <IconButton
+          style="primary"
+          icon="plus"
+          onClick={handleAdd}
+        ></IconButton>
+        <IconButton
+          style="info"
+          icon="search"
+          onClick={handleSearch}
+        ></IconButton>
+        <IconButton
+          style="default"
+          icon="close"
+          onClick={handleClear}
+        ></IconButton>
+      </Grid>
+    </div>
+  )
+}
