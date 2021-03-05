a5e0132a8fc9e5ec6f22523c6f17e5156423e1b5
renatogual <renatogual@hotmail.com>
2021-03-05 15:25:14 -0300

    Excluindo pagina sobre

--- frontend/src/about/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PageHeader from '../template/PageHeader'
-
-export default () => (
-  <div>
-    <PageHeader name="Sobre" small="Aplicação" />
-    <p>Aplicação simples para praticar a utilização do framework React</p>
-  </div>
-)
